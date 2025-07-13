b504a443309e612c16b10e5b842a58f07044a1e5
ignotus <darylo1@hotmail.com>
2025-07-13 15:59:11 +0200

    Add files via upload

---
+++ repo_c_01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhanlon <dhanlon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 16:43:45 by dhanlon           #+#    #+#             */
+/*   Updated: 2025/07/13 13:05:44 by dhanlon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int main(void)
+{
+    int value;
+    int *p1 = &value;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+    int ****p4 = &p3;
+    int *****p5 = &p4;
+    int ******p6 = &p5;
+    int *******p7 = &p6;
+    int ********p8 = &p7;
+    int *********p9 = &p8;
+
+    ft_ultimate_ft(p9);
+
+    printf("Value: %d\n", value); // Should print: Value: 42
+    return 0;
+}*/
